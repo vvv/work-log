ca4e10e7a2b32f1fe25d9a704ab0d22f74944461
Valery V. Vorotyntsev <valery.vv@gmail.com>
2009-04-17 00:26:17 +0300

    comments have been implemented
    + comments are being highlighted
    + comment/uncomment with `M-;'
    + use `font-lock-string-face' for dates

--- work-log.el
+++ work-log.el
@@ -1,8 +1,7 @@
-; (autoload 'work-log-mode "~/src/work-log/work-log.el" "XXX" t)
-
-; (setq work-log-font-lock-keywords
-(defvar work-log-font-lock-keywords
-  '("^[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}$")) ; XXX custom face
+(defvar work-log-font-lock-keywords nil "XXX")
+(setq work-log-font-lock-keywords
+      '(("^[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}$" . font-lock-string-face)
+	("^[[:space:]]*#.*$" . font-lock-comment-face)))
 
 ;;;###autoload
 (define-derived-mode work-log-mode text-mode "Work-Log"
@@ -15,12 +14,23 @@ Format was inspired by John Carmak's
   - entry was decided against on a later day
 
 \\{work-log-mode-map}"
-;;   (modify-syntax-entry ?# "<")
+  (set (make-local-variable 'comment-start) "#") ; enable `M-;'
   (set (make-local-variable 'font-lock-defaults)
-       '(work-log-font-lock-keywords t nil nil nil))
-  )
+       '(work-log-font-lock-keywords t nil nil nil)))
+
+(provide 'work-log)
+
+; XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+; (autoload 'work-log-mode "~/src/work-log/work-log.el" "XXX" t)
+; (unload-feature 'work-log)
 
 ;; (defcustom work-log-mode-hook nil
 ;;   "Normal hook run by `work-log-mode'."
 ;;   :type 'hook
 ;;   :group 'work-log)
+
+;; (defvar work-log-syntax-table
+;;   (let ((x (make-syntax-table)))
+;;     (modify-syntax-entry ?# "<" x)
+;;     (modify-syntax-entry ?\n ">" table)
+;;     x))
