4ea0b9bef5407ff620e66f0d7c43fc3b185233a7
Valery V. Vorotyntsev <valery.vv@gmail.com>
2009-07-05 13:51:11 +0300

    README added
    + genseq.hs: crop dead code

--- tests/genseq.hs
+++ tests/genseq.hs
@@ -1,8 +1,4 @@
-import Data.List
-
--- main = mapM_ putStr $ map ((++ "# --\n") . unlines)
---        $ permutations ["  a", "  a", "  + i", "  * i", "0000-00-00"]
+import Data.List (permutations)
 
 main = putStrLn $ concatMap (\(x,y) -> x++y) (xs `zip` repeat "-")
-    where
-      xs = concat $ permutations ["a", "a", "i", "i", "d"]
+    where xs = concat $ permutations ["a", "a", "i", "i", "d"]
